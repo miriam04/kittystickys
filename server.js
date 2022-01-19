4f94de0ee56c2661f1b6d01b66e6d17ede5affc9
miriam04 <maagm@bzz.ch>
2022-01-19 17:55:03 +0100

    make it work...

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ let {list} = require("./src/controller/NewsletterController")
 let app = express()
 
 app.engine('html', require('ejs').renderFile);
+app.use(express.static(__dirname + '/src/views'))
+app.use(express.static(__dirname + '/src/assets'))
 app.set('view engine', 'html');
 
 
