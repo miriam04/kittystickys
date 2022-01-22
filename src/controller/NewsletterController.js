7257637c1710cde47989a9aa365405c00b1b11df
miriam04 <maagm@bzz.ch>
2022-01-22 17:35:07 +0100

    finish

--- src/controller/NewsletterController.js
+++ src/controller/NewsletterController.js
@@ -2,7 +2,7 @@ const mysql = require("../utils/mysqlService")
 
 const create = async (req, res) => {
     mysql.create(req.body)
-    res.status(200).json({test: "test"})
+    res.status(200).json({success: true})
 }
 
 const read = async (req, res) => {
@@ -12,12 +12,12 @@ const read = async (req, res) => {
 
 const update = async (req, res) => {
     mysql.update(req.body)
-    res.status(200).json({test: "test"})
+    res.status(200).json({success: true})
 }
 
 const deleteById = async (req, res) => {
     mysql.delete(req.body.id)
-    res.status(200).json({test: "test"})
+    res.status(200).json({success: true})
 }
 
 module.exports = {create, read, update, deleteById}
